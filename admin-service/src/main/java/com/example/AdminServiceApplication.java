287142c14d03fc732c1f0fa66de5a63263955ada
bodyzxy <129066769+bodyzxy@users.noreply.github.com>
2024-04-14 23:33:05 +0800

    feign优化

--- admin-service/src/main/java/com/example/AdminServiceApplication.java
+++ admin-service/src/main/java/com/example/AdminServiceApplication.java
@@ -1,24 +1,17 @@
 package com.example;
 
+import com.example.clients.UserClients;
+import com.example.config.FeignClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(clients = UserClients.class,defaultConfiguration = FeignClientConfiguration.class)
 public class AdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
-
-//    @Bean
-//    @LoadBalanced
-//    RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
 }
